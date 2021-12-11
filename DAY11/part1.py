06246721f7c1491008a024c539a07e86931341b2
IlIll101 <thanoscarthanoscar420420@gmail.com>
2021-12-11 13:28:50 -0500

    easier

---
+++ DAY11/part1.py
@@ -0,0 +1,54 @@
+def rset(grid, n):
+    for i in range(len(grid)):
+        for y in range(len(grid)):
+            if grid[i][y] > 9:
+                grid[i][y] = 0
+                n += 1
+    return grid, n
+
+def flup(grid, a, c):
+    pl = len(a)
+    for i in range(len(grid)):
+        for y in range(len(grid)):
+            if grid[i][y] > 9 and [i, y] not in a:
+                a.append([i, y])
+                for x in ([i-1,y],[i+1,y],[i,y-1],[i,y+1],[i-1,y-1],[i-1,y+1],[i+1,y-1],[i+1,y+1]):
+                    try:
+                        if x[0] > -1 and x[1] > -1:
+                            grid[x[0]][x[1]] += 1
+                    except:
+                        pass
+    if len(a) == pl:
+        c = False
+
+    return grid, a, c
+                
+def inup(grid):
+    for i in range(len(grid)):
+        for y in range(len(grid[i])):
+            grid[i][y] += 1
+    return grid
+
+def init():
+    fin = [list(x) for x in open("input.txt", "r").read().split()]
+    for i in range(len(fin)):
+        fin[i] = [int(x) for x in fin[i]]
+    return fin
+
+def main():
+    fc = 0
+    grid = init()
+    for i in range(100):
+        ppd = []
+        c = True
+        grid = inup(grid)
+        while c == True:
+            grid, ppd, c = flup(grid, ppd, c)
+        grid, fc = rset(grid, fc)
+
+    print(fc)
+
+main()
+
+
+
