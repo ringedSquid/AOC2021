bf618719bdfd3657d955f949ef3815dfe2644552
IlIll101 <thanoscarthanoscar420420@gmail.com>
2021-12-15 21:24:03 -0500

    fuck it, im wasting my time

--- DAY15/part1.py
+++ DAY15/part1.py
@@ -1,54 +1,50 @@
-def path(adj, visited, sums, node, dest, grid):
-    risk = 0
-    ld = [100, 10]
-    for i in adj[node]:
-        if visited[i] == False:
-            if grid[i] < ld[1]:
-                ld = [i, grid[i]]
-            if grid[i] == grid[ld[0]]:
-                if sums[i] < sums[ld[0]]:
-                    ld = [i, grid[i]]
-            visited[i] = True
-    risk += ld[1]
-    print(ld)
-    risk += path(adj, visited, sums, ld[0], dest, grid)
-    return risk
+from queue import PriorityQueue
 
-
-
-
+def dij(node, adj, weights, visited):
+    costs = {x:float('inf') for x in adj}
+    costs[node] = 0
+    pq = PriorityQueue()
+    pq.put((0, node))
+    #sortedmap = sorted(visited.keys(), key=lambda e: distance(e, node)) 
+    while not pq.empty():
+        (useless, cnode) = pq.get()
+        visited[cnode] = True
+        for i in adj[cnode]:
+            if weights[i] != -1:
+                dist = weights[i]
+                if visited[i] == False:
+                    oldcost = costs[i]
+                    newcost = costs[cnode] + dist
+                    #print(oldcost, newcost, dist)
+                    if newcost < oldcost:
+                        pq.put((newcost, i))
+                        costs[i] = newcost
+    return costs
 
 
 def init():
     adj = {}
+    weights = {}
     visited = {}
-    sums = {}
-    grid = {}
-    fin = [[int(i) for i in list(x)] for x in open("tnput.txt", "r").read().split()]
-    end = (len(fin)-1, len(fin[0])-1)
+    fin = [[int(i) for i in list(x)] for x in open("input.txt", "r").read().split()] 
+    end = (len(fin[0])-1, len(fin)-1)
     for i in range(len(fin)):
         for y in range(len(fin[i])):
             if (y, i) not in adj:
                 adj[(y, i)] = []
                 visited[(y, i)] = False
-                grid[(y, i)] = fin[i][y]
-            if y > 0: adj[(y, i)].append((y-1, i))
-            if y < len(fin[i])-1: adj[(y, i)].append((y+1, i))
-            if i > 0: adj[(y, i)].append((y, i-1))
-            if i < len(fin)-1: adj[(y, i)].append((y, i+1))
+                weights[(y, i)] = fin[i][y]
+                if y > 0: adj[(y, i)].append((y-1, i))
+                if y < len(fin[i])-1: adj[(y, i)].append((y+1, i))
+                if i > 0: adj[(y, i)].append((y, i-1))
+                if i < len(fin)-1: adj[(y, i)].append((y, i+1))
 
-    for i in adj:
-        if i not in sums:
-            sums[i] = 0
-        for y in adj[i]:
-            sums[i] += fin[y[1]][y[0]]
-
-    return adj, visited, sums, end, grid 
+    return adj, weights, visited, end
 
 def main():
-    adj, visited, sums, end, grid = init()
-    visited[(0, 0)] = True
-    print(path(adj, visited, sums, (0, 0), end, grid)) 
-
+    adj, weights, visited, end  = init()
+    costs = dij((0,0), adj, weights, visited)
+    print(costs[end])
+    #print(costs)
 
 main()
